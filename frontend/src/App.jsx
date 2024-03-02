cac2cd5ad7482cc17f044339c00c3c61115ddb17
sreesanker <sreeshankerthekkan@gmail.com>
2024-03-02 20:32:10 +0530

    second commit setiing user friendly semester wise calculation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState } from 'react'
+import './app.scss'
+import Semester2 from './component/Semester2'
+import Semester1 from './component/Semester1'
+import Semester3 from './component/Semester3'
+import Semester4 from './component/Semester4'
+import Semester5 from './component/Semester5'
+import Semester6 from './component/Semester6'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+  const [data1,setData1] = useState([])
+  const [data2,setData2] = useState([])
+  const [cgpa,setCGPA] = useState('')
+
+ const handleCallback = (data1,data2)=>{
+
+    setData1(preve => [...preve,data1])
+
+    setData2(preve => [...preve,data2])
+ }   
+
+
+
+ const handleCgpa =()=>{
+    let netGpa = data1.reduce((a,initialvalue)=>(
+        a + initialvalue
+    ))
+console.log(netGpa);
+    let totalCredit = data2.reduce((a,initialvalue)=>(
+        a + initialvalue
+    ))
+    console.log(totalCredit);
+ 
+    setCGPA(netGpa / totalCredit)
+ }
+
 
   return (
-    <>
-     
-    </>
+ <div className='cgpaContainer'>
+  <div className='h3wrapper'>
+    <p>"Enter the grade point and credit for one subject, then click the 'Submit' button. Repeat this process until all subjects are completed. After the last submission, Click the 'Finish' button to complete the semester."</p>
+
+  </div>
+  <div className='semesterWrapper'>
+
+  <div className='3semester'>
+<Semester1 handleCallback={handleCallback}/>
+<Semester2 handleCallback={handleCallback}/>
+<Semester3 handleCallback={handleCallback}/>
+
+  </div>
+  <div className='3semester'>
+
+<Semester4 handleCallback={handleCallback}/>
+<Semester5 handleCallback={handleCallback}/>
+<Semester6 handleCallback={handleCallback}/>
+
+  </div>
+  </div>
+
+<div className='btnConatiner'>
+    <button onClick={handleCgpa}>submit data</button>
+</div>
+
+<h1>{cgpa}</h1>
+ </div>
   )
 }
-
-export default App
---
+++ frontend/src/component/Semester6.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+
+export default function Semester6 ({ handleCallback }) {
+  const [data1, setData1] = useState({
+    grade: null,
+    credit: null
+  })
+  const [ceditTotal, setCreditTotal] = useState([])
+  const [total, setTotal] = useState([])
+  const [visble, setVisibel] = useState(false)
+
+  const handleChange1 = e => {
+    let value = e.target.value.toUpperCase()
+    setData1({ ...data1, [e.target.id]: value })
+  }
+  const handleSubmit = () => {
+    if (
+      ['S', 'A', 'B', 'C', 'D', 'E'].includes(data1.grade) &&
+      !isNaN(data1.credit)
+    ) {
+      let gradeValue
+      switch (data1.grade) {
+        case 'S':
+          gradeValue = 10
+          break
+        case 'A':
+          gradeValue = 9
+          break
+        case 'B':
+          gradeValue = 8
+          break
+        case 'C':
+          gradeValue = 7
+          break
+        case 'D':
+          gradeValue = 6
+          break
+        case 'E':
+          gradeValue = 5
+          break
+        default:
+          break
+      }
+      const a = parseInt(gradeValue, 10)
+      const b = parseInt(data1.credit, 10)
+      const ans = a * b
+      setCreditTotal(preve => [...preve, b])
+      setTotal(prevTotal => [...prevTotal, ans])
+      setData1({
+        grade: '',
+        credit: ''
+      })
+    } else {
+      alert('enter valid grade or valid cedit data')
+    }
+  }
+
+  const handleFinish = () => {
+    const data1 = total.reduce(
+      (acumilator, correntValue) => acumilator + correntValue
+    )
+    const data2 = ceditTotal.reduce(
+      (acumilator, correntValue) => acumilator + correntValue
+    )
+
+    handleCallback(data1, data2)
+    setVisibel(true)
+  }
+  return (
+    <div className='mainWrapper'>
+      <div className='wrapper'>
+        {!visble ? (
+          <>
+            {' '}
+            <label htmlFor=''>semester 6</label>
+            <input
+              id='grade'
+              value={data1.grade}
+              onChange={handleChange1}
+              type='text'
+              placeholder='grade '
+            />
+            <input
+              id='credit'
+              value={data1.credit}
+              onChange={handleChange1}
+              type='text'
+              placeholder='credit'
+            />
+            <button onClick={handleSubmit}>submit</button>
+            <button onClick={handleFinish}>finish</button>
+          </>
+        ) : (
+          <h3>semester 6 comppeted    <FontAwesomeIcon icon={faSquareCheck} />  </h3> 
+          
+          
+        )}
+      </div>
+     
+    </div>
+  )
+}
